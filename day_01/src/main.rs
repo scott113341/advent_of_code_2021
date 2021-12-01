27e52ed313fc4a815d52f1531476817b1db78332
Scott Hardy <scott.the.hardy@gmail.com>
2021-12-01 09:10:28 -0700

    Day 1

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,7 +1,5 @@
-mod data;
 mod input;
 
-use data::*;
 use input::*;
 
 fn main() {
@@ -10,12 +8,47 @@ fn main() {
     println!("  part 2: {}", part_2(get_input()));
 }
 
-fn part_1(lines: Vec<String>) -> usize {
-    1
+// Count the number of times a depth measurement increases from the previous measurement
+fn part_1(readings: Vec<usize>) -> usize {
+    let mut previous_reading = None;
+    let mut increase_count = 0;
+
+    for reading in readings {
+        if let Some(previous_reading) = previous_reading {
+            if reading > previous_reading {
+                increase_count += 1;
+            }
+        }
+
+        previous_reading = Some(reading);
+    }
+
+    increase_count
 }
 
-fn part_2(things: Vec<Thing>) -> usize {
-    2
+// Sum three-measurement sliding windows. How many sums are larger than the previous sum?
+fn part_2(readings: Vec<usize>) -> usize {
+    let mut previous_window_sum = None;
+    let mut increase_count = 0;
+
+    for (idx, reading) in readings.iter().enumerate() {
+        // Stop when there's no longer a window of 3 readings remaining
+        if readings.get(idx + 2).is_none() {
+            break;
+        }
+
+        let window_sum = reading + readings[idx + 1] + readings[idx + 2];
+
+        if let Some(previous_window_sum) = previous_window_sum {
+            if window_sum > previous_window_sum {
+                increase_count += 1;
+            }
+        }
+
+        previous_window_sum = Some(window_sum);
+    }
+
+    increase_count
 }
 
 #[cfg(test)]
@@ -24,11 +57,11 @@ mod tests {
 
     #[test]
     fn test_part_1() {
-        assert_eq!(part_1(get_test_input()), 1);
+        assert_eq!(part_1(get_test_input()), 7);
     }
 
     #[test]
     fn test_part_2() {
-        assert_eq!(part_2(get_test_input()), 2);
+        assert_eq!(part_2(get_test_input()), 5);
     }
 }
